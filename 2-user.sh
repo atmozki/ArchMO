324b9b9aae263088247c53e3ae522d2b92ec798f
Dennis Jojo Kuriakose <dennisjojo052001@gmail.com>
2021-11-27 10:49:55 +0530

    added option to skip Konsave

--- 2-user.sh
+++ 2-user.sh
@@ -67,11 +67,17 @@ for PKG in "${PKGS[@]}"; do
     yay -S --noconfirm $PKG
 done
 
-yay -S --noconfirm konsave
-pip install konsave
-konsave -i $HOME/ArchMO/atmoz.knsv
-sleep 1
-konsave -a atmoz
+read -p "Do you want to restore KONSAVE? [y/n]" answer
+if [[ $answer = y ]] ; then
+    yay -S --noconfirm konsave
+    pip install konsave
+    konsave -i $HOME/ArchMO/atmoz.knsv
+    sleep 1
+    konsave -a atmoz
+fi
+if [[ $answer = n ]] ; then
+    echo "Skipping KONSAVE"
+fi
 
 echo -e "\nDone!\n"
 exit
--- kderice-restore.sh
+++ kderice-restore.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-yay -S --noconfirm konsave
-pip install konsave
-konsave -i $HOME/ArchMO/atmoz.knsv
-sleep 1
-konsave -a atmoz
+read -p "Do you want to restore KONSAVE? [y/n]" answer
+if [[ $answer = y ]] ; then
+    yay -S --noconfirm konsave
+    pip install konsave
+    konsave -i $HOME/ArchMO/atmoz.knsv
+    sleep 1
+    konsave -a atmoz
+fi
+if [[ $answer = n ]] ; then
+    echo "NOOO"
+fi
